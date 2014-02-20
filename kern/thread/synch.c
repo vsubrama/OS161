97588f8d35deb55c0ca0ee755549a4a06d3bcb49
ops-class.org Student <trinity@zion.(none)>
2014-02-20 14:00:25 -0500

    Vasanth rwlock bug fix6

--- kern/thread/synch.c
+++ kern/thread/synch.c
@@ -381,7 +381,7 @@ rwlock_acquire_read(struct rwlock *rwlock)
 		KASSERT(rwlock != NULL);
 		kprintf("Inside rwlock_acquire_read \n");
 			spinlock_acquire(&rwlock->rwspn_lock);
-			while (rwlock->writer)
+			while (!wchan_isempty(rwlock->wlock_wchan))
 				{
 					wchan_lock(rwlock->rlock_wchan);
 					spinlock_release(&rwlock->rwspn_lock);
@@ -402,7 +402,7 @@ rwlock_release_read(struct rwlock *rwlock)
 		rwlock->num_reader = rwlock->num_reader - 1;
 		kprintf("Inside rwlock_release_read reader decremented\n");
 		}
-		if ((rwlock->num_reader == 0))
+		if ((rwlock->num_reader == 0) && (!wchan_isempty(rwlock->wlock_wchan)))
 		{
 			kprintf("Inside rwlock_release_read reader 0\n");
 			wchan_wakeall(rwlock->wlock_wchan);
